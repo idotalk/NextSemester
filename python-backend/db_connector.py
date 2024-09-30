bceff8a03aeeee685d880d5bf98e6f72eca743a8
idotalk <idotalke19@gmail.com>
2024-09-30 23:32:58 +0300

    deployment changes

--- python-backend/db_connector.py
+++ python-backend/db_connector.py
@@ -1,11 +1,17 @@
 import psycopg2
-
+import os
 
 def estconnection():
+        db_name= os.environ['RENDER_DATABASE']
+        db_host = os.environ['RENDER_HOST']
+        db_password = os.environ['RENDER_PASS']
+        db_port = os.environ['RENDER_PORT']
+        db_username = os.environ['RENDER_USER']
         conn = psycopg2.connect(
-                host="localhost",
-                port="5432",
-                database="cs_courses",
-                user="postgres",
-                password="1297")
+                host=db_host,
+                port=db_port,
+                database=db_name,
+                user=db_username,
+                password=db_password,
+                sslmode='require')
         return conn
