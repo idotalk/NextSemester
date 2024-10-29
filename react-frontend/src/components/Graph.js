181d3be7254a1486c1e720a7e6869e7c15aab06c
Ido talker <129003699+idotalk@users.noreply.github.com>
2024-10-29 22:03:47 +0200

    Graph edges data moved from static to dynamic

--- react-frontend/src/components/Graph.js
+++ react-frontend/src/components/Graph.js
@@ -1,14 +1,80 @@
-import React, { useState, useRef } from "react";
+
+import React, { useState, useRef, useEffect } from "react";
 import Graph from "react-vis-network-graph";
 import "./Graph.css";
 import { FaSearch } from "react-icons/fa";
 import NodeIndex from "./NodeIndex.js";
-import { nodes, edges } from "./graphdata.js";
+import { nodes } from "./graphdata.js";
+import axios from "axios";
 
 function Graphcomp() {
+  const pair_courses_data = [
+    { "from": 104031, "to": 114071, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 104032, "to": 234125, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 94412,  "to": 234218, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 234118, "to": 234123, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 104135, "to": 104032, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 104032, "to": 114075, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 236272, "to": 236271, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 236332, "to": 236333, dashes:true, arrows: { "to": { enabled: false } }},
+    { "from": 236350, "to": 236490, dashes:true, arrows: { "to": { enabled: false } }},
+  ];
+
+  const [prerequisites_data, setPreData] = useState([]);
   const [graph, setGraph] = useState({ nodes, edges });
   const [searchId, setSearchId] = useState("");
   const networkRef = useRef(null);
+  
+  useEffect(() => {
+    axios
+      .get("RestfulApiUrl/prerequisites")
+      .then((response) => {
+        setPreData(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const combinedEdges = [...pair_courses_data, ...prerequisites_data];
+    setGraph({ nodes, edges: combinedEdges });
+  }, [prerequisites_data]);
+  
+  const option = {
+    autoResize: true,
+    nodes: {
+      shape: "dot",
+      scaling: {
+        min: 10,
+        max: 30,
+      },
+      font: {
+        color: "#343434",
+        size: 12,
+        face: "Tahoma",
+      },
+    },
+    edges: {
+      color: "black",
+      width: 0.4,
+      length: 150,
+      smooth: {
+        type: "continuous",
+      },
+    },
+    physics: {
+      forceAtlas2Based: {
+        gravitationalConstant: -50,
+        centralGravity: 0.005,
+        springConstant: 0.08,
+      },
+      solver: "forceAtlas2Based",
+      maxVelocity: 50,
+    },
+    height: "100%",
+    width: "100%",
+  };
 
   const option = {
     autoResize: true,
